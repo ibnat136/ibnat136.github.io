dd0554413e5d49981e84017cd06b328db6db7ae5
ibnat136 <53865464+ibnat136@users.noreply.github.com>
2021-12-10 12:09:14 -0500

    Add files via upload

---
+++ BackpackApp/Component/data.js
@@ -0,0 +1,35 @@
+import Backpack from "./Backpack.js";
+
+
+const everydayPack = new Backpack(
+  "pack01",
+  "Everyday Backpack",
+  30,
+  "grey",
+  15,
+  26,
+  26,
+  false,
+  "December 5, 2018 15:00:00 PST",
+  "../assets/images/everyday.svg"
+);
+
+
+const frogPack = new Backpack(
+  "pack02",
+  "Frog Backpack",
+  8,
+  "green",
+  3,
+  10,
+  10,
+  false,
+  "October 16, 2019 15:00:00 PST",
+  "../assets/images/frog.svg"
+);
+
+// Add Backpack objects into an array
+const backpackObjectArray = [everydayPack, frogPack];
+
+// Export the array to be used in other files
+export default backpackObjectArray;
